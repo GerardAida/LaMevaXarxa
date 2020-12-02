e7fb524eb1d60e2fad5497487ad3eea46c28f257
AidaJesusSora <ajsora97@gmail.com>
2020-12-02 17:07:52 +0100

    Llista usuaris
    Creació de la llista d'usuaris juntament amb el botó d'eliminar de manera funcional

---
+++ src/app/model/llista-usuari.model.ts
@@ -0,0 +1,12 @@
+export class LlistaUsuari {
+
+    constructor(
+        public nom: string,
+        public cognoms: string,
+        public edat: string,
+        public descripcio: string,
+        public foto: string
+    ) { }
+  
+  }
+  
--- src/app/panell-admin/llista-usuari/llista-usuari.component.ts
+++ src/app/panell-admin/llista-usuari/llista-usuari.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LlistaUsuari } from 'src/app/model/llista-usuari.model';
 
 @Component({
   selector: 'app-llista-usuari',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LlistaUsuariComponent implements OnInit {
 
+  usuaris: LlistaUsuari[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.usuaris.push(new LlistaUsuari('Samuel','Orellana Pi', '22', 'Gran persona' ,'assets/img/usuaris-img/persona-1.jpg'));
+    this.usuaris.push(new LlistaUsuari('Josep','Guardia Peralta', '45', 'Apasionat de les motos' ,'assets/img/usuaris-img/persona-2.jpg'));
+    this.usuaris.push(new LlistaUsuari('Anna','Silva Martinez', '20', 'Autodidacta' ,'assets/img/usuaris-img/persona-3.jpg'));
+    this.usuaris.push(new LlistaUsuari('Anabel','Sora Cervero', '25', 'Superheroina' ,'assets/img/usuaris-img/persona-4.jpg'));
+    this.usuaris.push(new LlistaUsuari('Jannete','LeBlanc', '23', 'Amant del vi' ,'assets/img/usuaris-img/persona-5.png'));
+  }
+
+  eliminar(llista_usuari_model) {
+    this.usuaris.splice(llista_usuari_model,1);
   }
 
 }
