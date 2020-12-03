000a96b8cecb0ca8064c6505fcce2d4607c44e62
GerardRey <g.rey36500@gmail.com>
2020-12-04 00:11:18 +0100

    Registre Usuari Update
    Se a afegit casi totes les funcionalitats que a de tenir el Registre

--- src/app/perfil-usuari/registre-usuari/registre-usuari.component.ts
+++ src/app/perfil-usuari/registre-usuari/registre-usuari.component.ts
@@ -1,89 +1,75 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { FormsModule,FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LlistaPefils } from 'src/app/model/llista-perfils.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registre-usuari',
   templateUrl: './registre-usuari.component.html',
   styleUrls: ['./registre-usuari.component.css']
 })
+
 export class RegistreUsuariComponent implements OnInit {
 
-  nombre: string ="";
-  lloc: string ="";
+  nom: string ="";
+  cognom: string ="";
+  edat: number=0;
+  foto: string ="";
+  descripcio: string="";
   correu: string="";
-  descrip: string="";
+  password: string="";
 
-  mailPatt= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  @Output() pasarArray = new EventEmitter<LlistaPefils>();
 
-  @Output() pasarArray = new EventEmitter<GrupEvents>();
+  constructor(private formBuilder: FormBuilder) { }
 
-  errors: boolean[] = [false, false, false, false];
-  error : number = 0;
-
-
-  constructor() { }
+  //creo la variable usuari donde almaceno los datos que recibo del formulario
+  usuari: FormGroup;
+  submitted = false;
 
   ngOnInit(): void {
+        //pongo las condiciones de los campos
+        this.usuari = this.formBuilder.group({
+
+          nom: ['', Validators.required],
+          correu: ['', [Validators.required, Validators.email]]
+
+        });
   }
 
-  EnviarDatos(nombre,lloc,correu,descrip,checked): void {
-    console.log(checked)
-    this.error = 0;
-    if (nombre === "") {
-      this.errors[0] = true
-      this.error++;
-    }
-    else {
-      this.errors[0] = false;
-    }
-
-    if (lloc === "") {
-      this.errors[1] = true
-      this.error++;
-    }
-    else {
-      this.errors[1] = false;
-    }
-
-    if (descrip.length < 2) {
-      this.errors[2] = true
-      this.error++;
-    }
-    else {
-      this.errors[2] = false;
-    }
-
-    if(!correu.match(this.mailPatt)) {
-      this.errors[3] = true
-      this.error++;
-    }
-    else {
-      this.errors[3] = false;
-    }
-
-    if(!checked) {
-      this.errors[4] = true
-      this.error++;
-    }
-    else {
-      this.errors[4] = false;
-    }
-
-    if(this.error === 0) {
-      this.GuardarDatos(nombre,lloc,correu,descrip)
-    }
-
+  // creamos un metodo para facilitar el acceso a los controles de formulario desde la plantilla
+  get f(){
+    return this.usuari.controls;
   }
 
-  GuardarDatos(nombre,lloc,correu,descrip): void {
-    this.nombre = nombre;
-    this.lloc = lloc;
+
+  //metodo enviar
+  enviarDatos(nom,cognom,edat,foto,descripcio,correu,password){
+
+    this.submitted = true;
+    //si algun campo no cumple las condiciones
+    if(this.usuari.invalid){
+      return;
+    }
+
+    //si todos los campos son correctos muestra la siguiente ventana emergente
+    this.GuardarDatos(nom,cognom,edat,foto,descripcio,correu,password);
+    Swal.fire('Los datos son correctos');
+  }
+
+  GuardarDatos(nom,cognom,edat,foto,descripcio,correu,password): void {
+    this.nom = nom;
+    this.cognom = cognom;
+    this.edat = edat;
+    this.foto = foto;
+    this.descripcio = descripcio;
     this.correu = correu;
-    this.descrip = descrip;
+    this.password = password;
     this.EnviarAlPadre();
   }
 
   EnviarAlPadre(): void {
-    this.pasarArray.emit(new GrupEvents(this.nombre, this.lloc, this.correu, this.descrip));
+    this.pasarArray.emit(new LlistaPefils(this.nom, this.cognom, this.edat, this.foto, this.descripcio, this.correu, this.password));
 
   }
 
