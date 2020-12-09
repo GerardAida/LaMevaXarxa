25a1e2c47f65cec15e35cb7f350a37fd4b4d1cfd
DAW2 <72407398+GerardRey@users.noreply.github.com>
2020-12-09 15:50:39 +0100

    Correcions manies Aida
    S'ha corregit el codi segons les manies de l'Aida

--- src/app/perfil-usuari/llista-perfils/llista-perfils.component.ts
+++ src/app/perfil-usuari/llista-perfils/llista-perfils.component.ts
@@ -14,9 +14,7 @@ export class LlistaPerfilsComponent implements OnInit {
   constructor() {
     console.log("a")
     this.usuaris.push(new LlistaPefils('Samuel','Orellana Pi', 22, 'assets/img/usuaris-img/persona-1.jpg', 'Gran persona', 'samuel@gmail.com', '3,14159265358979323846...'));
-    this.usuaris.push(new LlistaPefils('Josep','Guardia Peralta', 45, 'assets/img/usuaris-img/persona-2.jpg', 'Apasionat de les motos', 'josep@gmail.com', 'motorola'));
     this.usuaris.push(new LlistaPefils('Anna','Silva Martinez', 20, 'assets/img/usuaris-img/persona-3.jpg', 'Autodidacta', 'anna@gmail.com','240800'));
-    this.usuaris.push(new LlistaPefils('Anabel','Sora Cervero', 25, 'assets/img/usuaris-img/persona-4.jpg', 'Superheroina', 'anabel@gmail.com','CapitanaMarvel'));
     this.usuaris.push(new LlistaPefils('Jannete','LeBlanc', 23, 'assets/img/usuaris-img/persona-5.png', 'Amant del vi', 'jannete@gmail.com','En invierno no hay mal abrigo con una copa de un buen vino'));
 
    }
